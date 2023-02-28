dc1f937def2becf571d54394fac0816d499ecfeb
Iván González <ivan.gonzalez@campusfp.es>
2023-02-28 12:36:38 +0100

    initial commit

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'Ev2',
-  webDir: 'www',
+  webDir: 'src',
   bundledWebRuntime: false
 };
 
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,10 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, FormControl, AbstractControl } from '@angular/forms';
+interface FormData {
+  fullName: AbstractControl<any, any>;
+  email: AbstractControl<any, any>;
+  password: AbstractControl<any, any>;
+}
 
 @Component({
   selector: 'app-tab1',
@@ -6,7 +12,19 @@ import { Component } from '@angular/core';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
+  myForm: FormGroup<FormData>;
+  
+  constructor(private formBuilder: FormBuilder) {
+    this.myForm = this.formBuilder.group<FormData>({
+      email: new FormControl('', [Validators.required, Validators.email, Validators.pattern(/^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/)]), 
+      password: new FormControl('', [Validators.required, Validators.minLength(6), Validators.maxLength(12), Validators.pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{6,12}$/)]),
+      fullName: new FormControl('', [Validators.required, Validators.minLength(3), Validators.maxLength(30), Validators.pattern(/^[a-zA-Z\s]+$/) ])
+    });
+  }
+  
 
-  constructor() {}
 
+  submitForm() {
+    alert('Gracias por unirte a nuestra comunidad')
+  }
 }
